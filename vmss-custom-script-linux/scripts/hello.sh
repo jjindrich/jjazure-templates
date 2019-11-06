b4f0021f1794906171afaec587293fcf3685b93c
Jaroslav Jindřich <jaroslav.jindrich@outlook.com>
2019-11-06 11:01:09 -0500

    Update hello.sh

--- vmss-custom-script-linux/scripts/hello.sh
+++ vmss-custom-script-linux/scripts/hello.sh
@@ -1,4 +1,6 @@
-apt-get -y update
-apt-get install -y mc
-apt-get install -y docker.io
+until apt-get -y update && apt-get -y install docker.io
+do
+ echo "Try again"
+ sleep 2
+done
 docker run -d --restart="always" -p 80:80 yeasy/simple-web
